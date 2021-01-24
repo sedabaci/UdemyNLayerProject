412fee424f65cf1b82109ee8e26a43dcfe5079f7
SedaBacı <sedabacii@gmail.com>
2021-01-24 20:52:35 +0300

    Add project files.

---
+++ UdemyNLayerProject.Core/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    public class Category
+    {
+        //code first: burda olusturulan modellerin db'de tabloya dönüştürülmesi
+        //db first:   db'de ki tabloların burada modele cevrilmesi.
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; } 
+        // bire cok ilişki var. her category nin birden cok productı olabilir. bos bir collection dizisi olusturduk. 
+    }
+}
---
+++ UdemyNLayerProject.Core/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarkod { get; set; }
+        public virtual Category Category{ get; set; }  
+        // Category classına referans veriyoruz. entity Category sınıfının üzerinden inherit kullanarak değişiklikleri izlemesi için "virtual" kullandık.
+    }
+}
