944b7b698e85ee841e76df2262c933b85589d3a6
SedaBacı <sedabacii@gmail.com>
2021-01-30 21:54:02 +0300

    ICategoryRepository ve IProductRepository interfaceleri implemente edildi

--- UdemyNLayerProject.Core/Repositories/IRepository.cs
+++ UdemyNLayerProject.Core/Repositories/IRepository.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace UdemyNLayerProject.Core.Repositories
 {
-    public interface IRepository<TEntity> where TEntity: class
+    public interface IRepository<TEntity> where TEntity : class
     {
         //generic repository pattern (crud islemleri gibi tüm entityler için gerekli islemler burada)
 
-        Task<TEntity> GetByIdAsync(int id);        
+        Task<TEntity> GetByIdAsync(int id);
         Task<IEnumerable<TEntity>> GetAllAsync();
         //herhangi parametreye göre ilgili nesneleri bul
-        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         //herhangi bir nesnesine göre getir
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
-        Task AddAsync(TEntity entity);        
+        Task AddAsync(TEntity entity);
         Task AddRangeAsync(IEnumerable<TEntity> entities);
         void Remove(TEntity entity);
-        void RemoveRange(IEnumerable<TEntity> entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
         TEntity Update(TEntity entity);
     }
 }
---
+++ UdemyNLayerProject.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        /// <summary>
+        /// repositoryden gelen _context i AppDbContext'e convert ettik
+        /// </summary>
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await _appDbContext.Products.Include(x => x.Category).SingleOrDefaultAsync(x => x.Id == productId);
+        }
+    }
+}
