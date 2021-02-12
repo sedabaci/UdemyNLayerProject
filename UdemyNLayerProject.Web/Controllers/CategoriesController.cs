f41f0030e8c0694bcd9f0e83445cc0985f1a6c90
SedaBacı <sedabacii@gmail.com>
2021-02-12 13:35:20 +0300

    CategoriesController classı olusturuldu

---
+++ UdemyNLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using UdemyNLayerProject.Core.Services;
+
+namespace UdemyNLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper; 
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
