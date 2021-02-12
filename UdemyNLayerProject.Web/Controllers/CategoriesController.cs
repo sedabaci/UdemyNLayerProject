a7b5718e01137792e6935fed9cee7f02778453cc
SedaBacı <sedabacii@gmail.com>
2021-02-12 18:58:40 +0300

    CategoryController Delete ve Update Action methodları olusturuldu

--- UdemyNLayerProject.Web/Controllers/CategoriesController.cs
+++ UdemyNLayerProject.Web/Controllers/CategoriesController.cs
@@ -15,7 +15,7 @@ namespace UdemyNLayerProject.Web.Controllers
         /// index sayfasında categorylerimi görüntülemek istiyorum
         /// </summary>
         private readonly ICategoryService _categoryService;
-        private readonly IMapper _mapper; 
+        private readonly IMapper _mapper;
         public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
@@ -26,7 +26,6 @@ namespace UdemyNLayerProject.Web.Controllers
             var categories = await _categoryService.GetAllAsync();
             return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
         }
-
         public IActionResult Create()
         {
             return View(); //sayfa yüklendiğinde ilk calısacak methodum
@@ -38,7 +37,25 @@ namespace UdemyNLayerProject.Web.Controllers
             await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
             return RedirectToAction("Index");
         }
+        public async Task<IActionResult> Update(int id)
+        {
+            //sayfa yüklendiği an client'dan aldığım id'ye ait categori bilgilerini alıyorum
+            var category = await _categoryService.GetByIdAsync(id);
+            return View(_mapper.Map<CategoryDto>(category));
+        }
 
+        [HttpPost]
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            _categoryService.Update(_mapper.Map<Category>(categoryDto));            
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var category = _categoryService.GetByIdAsync(id).Result;
+            _categoryService.Remove(category);
+            return RedirectToAction("Index");
+        }
 
     }
 }
